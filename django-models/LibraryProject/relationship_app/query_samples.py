53a1ae173bb08a0094e30ef95f41b2e19281b12f
russellbelmont <russev.odhiambo@gmail.com>
2025-09-05 11:37:51 +0300

    Fix query_samples.py with explicit queries for checkers

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,9 @@
-from relationship_app.models import Author, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.book_set.all()
+    return Book.objects.filter(author=author)
 
 # List all books in a library
 def books_in_library(library_name):
@@ -11,6 +11,6 @@ def books_in_library(library_name):
     return library.books.all()
 
 # Retrieve the librarian for a library
-def librarian_of_library(library_name):
+def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
