de4e25c3274ed9ba1d8ce0d6d67aca4403d09cb9
russellbelmont <russev.odhiambo@gmail.com>
2025-09-05 13:11:06 +0300

    Fix imports, class-based view, urls, and template paths for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -51,3 +51,42 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm  # we’ll create this form next
+
+
+@permission_required("relationship_app.can_add_book")
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_books")
+    else:
+        form = BookForm()
+    return render(request, "relationship_app/add_book.html", {"form": form})
+
+
+@permission_required("relationship_app.can_change_book")
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("list_books")
+    else:
+        form = BookForm(instance=book)
+    return render(request, "relationship_app/edit_book.html", {"form": form})
+
+
+@permission_required("relationship_app.can_delete_book")
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect("list_books")
+    return render(request, "relationship_app/delete_book.html", {"book": book})
